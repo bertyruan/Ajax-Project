c72b3e70bd24493c8dae791ee28aa79ebc216fab
chinesebaloni <berty.ruan@gmail.com>
2015-06-14 22:39:39 -0400

    Display nytime articles headline and snippets on web page
    Has error handling for bad nytime api requests.

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ function loadData() {
     
     var $street  = $('#street').val();
     var $city    = $('#city').val();
-    var $address =  $street + " , " + $city;
+    var $address =  $street + ", " + $city;
     var $streetView = "<img class='bgimg' src='https://maps.googleapis.com/maps/api/streetview?size=600x400&location=" + $address + "'>"
     
     // load streetview
@@ -23,7 +23,7 @@ function loadData() {
     // change titles to match query
     $greeting.text("So you want to live at " + $address + "?"); 
     $nytHeaderElem.text("New York Times Articles at " + $address);
-    
+
 
     var nytUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=";
     var apiKey = "a23984d3ba0e636a13d3179baebd543f:2:72307059";
@@ -34,7 +34,15 @@ function loadData() {
 
     //New York Times article ajax
     $.getJSON(url, function(data){
-        //$.each(data, )
+        console.log(data);
+        $.each(data.response.docs, function(index, element){
+            console.log(element.snippet);
+            $("#nytimes-articles").append("<li class='article'>" +
+                "<a href='"+element.web_url+"'>" + element.headline.main + "</a>" +
+                "<p>" + element.snippet + "</p>") + "</li>";
+        });
+    }).error(function(){
+        $nytHeaderElem.text("Sorry :(");
     });
 
     //<li class="article"></li>
